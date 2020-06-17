e28ee9896f89d509adab9a7f51688389daa855c9
ann903 <1291749723@qq.com>
2020-06-18 00:38:51 +0800

    完成文件上传功能

---
+++ 20200525/index.js
@@ -0,0 +1,86 @@
+const Koa = require('koa');
+const app = new Koa();
+const router = require('koa-router')();
+const koaBody = require('koa-body');
+const serve = require('koa-static');
+const fs = require('fs');
+// router.post('/users', koaBody(),
+//   (ctx) => {
+//     console.log(ctx.request.body);
+//     // => POST body
+//     ctx.body = JSON.stringify(ctx.request.body);
+//   }
+// );
+
+// app.use('/upload',koaBody({
+//     multipart: true,
+//     formidable: {
+//         maxFileSize: 200*1024*1024	// 设置上传文件大小最大限制，默认2M
+//     }
+// }));
+
+
+// postData with koabody
+// router.post('/users', async (ctx) => {
+//     const file = ctx.request.body.files.file;	// 获取上传文件
+// 	const reader = fs.createReadStream(file.path);	// 创建可读流
+// 	const ext = file.name.split('.').pop();		// 获取上传文件扩展名
+// 	const upStream = fs.createWriteStream(`upload/${Math.random().toString()}.${ext}`);		// 创建可写流
+// 	reader.pipe(upStream);	// 可读流通过管道写入可写流
+// 	return ctx.body = '上传成功';
+// })
+
+
+// postData without koabody
+router.post('/users', async (ctx) => {
+  console.log(ctx.request.body);
+  // 解析上下文里node原生请求的POST参数
+  let parsedData = await new Promise((resolve, reject) => {
+    try {
+      let postdata = "";
+      ctx.req.addListener('data', (data) => {
+        postdata += data
+      })
+      ctx.req.addListener("end", function () {
+        // // console.log(parseData);
+        resolve(postdata);
+      })
+    } catch (err) {
+      reject(err)
+    }
+  })
+  // => POST body
+  // here we got the content of the file
+  ctx.body = parsedData;  
+
+  // so what we need to is saving the content to local file
+  // const reader = fs.createReadStream(parsedData);	// 创建可读流
+  // const ext = 'txt';		// 获取上传文件扩展名
+  // const upStream = fs.createWriteStream(`./static/upload/${Math.random().toString()}.${ext}`);		// 创建可写流
+  // reader.pipe(upStream);	// 可读流通过管道写入可写流
+
+  let result = await new Promise ((resolve, reject) => {
+    fs.writeFile(`./static/upload/${Math.random().toString()}.ann`, parsedData, 
+    function(err){
+      if(err) {
+        reject(err)
+      } else {
+        resolve('写文件操作成功');
+      } 
+    })
+  })
+  ctx.body = result;  
+ 
+});
+
+// router.get('/users', (ctx) => {
+//     console.log("user get");
+//     // => POST body
+//     ctx.body = "上传成功";
+//   }
+// );
+// app.use(require('koa-static')(static,opts));
+app.use(serve( __dirname,  './static'));
+app.use(router.routes());
+ 
+app.listen(3000);
---
+++ 20200525/service/index.js
@@ -0,0 +1,16 @@
+const serve = require('koa-static');
+const Koa = require('koa');
+const app = new Koa();
+ 
+// $ GET /package.json
+app.use(serve('.'));
+ 
+// $ GET /hello.txt
+app.use(serve('test/fixtures'));
+ 
+// or use absolute paths
+app.use(serve(__dirname + '/test/fixtures'));
+ 
+app.listen(3000);
+ 
+console.log('listening on port 3000');
